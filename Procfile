00d2cbce685a05b854719798dbc88791a769d70d
Muhammed MUsa <72840992+Galapagous@users.noreply.github.com>
2022-08-12 10:36:17 +0100

    first commit

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: node index.js
+web: node /client/build/index.js
--- index.js
+++ index.js
@@ -5,32 +5,39 @@ const TodoApi = require("./Routes/Todo")
 const cors = require("cors")
 const bodyParser = require("body-parser")
 const dotenv = require("dotenv")
+const path = require("path")
 
 App.use(cors())
 App.use(express.json())
 dotenv.config()
 
 mongoose
-  .connect(process.env.MONGO_URLU || process.env.SHELL_URL, { useUnifiedTopology: true, useNewUrlParser: true })
+  .connect(process.env.SHELL_URL, { useUnifiedTopology: true, useNewUrlParser: true })
   .then(console.log("connected to DB"))
   .catch((err) => {
-    console.log(`error connecting to database${err}`)
+    console.log(`error connecting to database:${err}`)
   })
 
-App.get("/", (req, res) => {
-  res.send("Hello from galapagous")
-})
 App.use("/api", TodoApi)
 
-// App.use(express.static(path.join(__dirname, "/client")))
-// App.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "/client/build", "index.js"))
-// })
+// ------------------------------Deployment------------------------------------
+const __dirname1 = path.resolve()
+if (process.env.NODE_ENV === "production") {
+  App.use(express.static(path.join(__dirname1, "client/build")))
+  App.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname1, "client", "build", "index.html"))
+  })
+} else {
+  App.get("/", (req, res) => {
+    res.send("Hello from galapagous")
+  })
+}
+// ------------------------------Deployment------------------------------------
 
 let port = process.env.PORT
 if (port == null || port == "") {
   port = process.env.MY_PORT
 }
-App.listen(process.env.PORT, () => {
-  console.log(`App is listening on port ${port}`)
+App.listen(4000, () => {
+  console.log(`App is listening on port 4000`)
 })
