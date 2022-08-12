bde2d873eac844c252c91e206befc039ec6a7e99
Muhammed MUsa <72840992+Galapagous@users.noreply.github.com>
2022-08-12 17:23:19 +0100

    another log commit

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const __dirname1 = path.resolve()
 if (process.env.NODE_ENV === "production") {
   App.use(express.static(path.join(__dirname, "/client/build")))
   App.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client/build", "index.html"))
+    res.sendFile(path.resolve(__dirname, "/client/build", "index.html"))
   })
 } else {
   App.get("/", (req, res) => {
